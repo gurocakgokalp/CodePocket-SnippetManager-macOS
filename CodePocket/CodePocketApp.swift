3253a2c6ebc40feddbdc6d8ae794dbe805c0c0df
gokalp <ggurocak58@gmail.com>
2025-11-27 21:41:23 +0300

    Initial Commit

---
+++ CodePocket/CodePocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodePocketApp.swift
+//  CodePocket
+//
+//  Created by Gökalp Gürocak on 27.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct CodePocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodePocketTests/CodePocketTests.swift
@@ -0,0 +1,17 @@
+//
+//  CodePocketTests.swift
+//  CodePocketTests
+//
+//  Created by Gökalp Gürocak on 27.11.2025.
+//
+
+import Testing
+@testable import CodePocket
+
+struct CodePocketTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
